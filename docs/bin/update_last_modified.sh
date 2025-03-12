e375b0990a73bd4d2b1f24c07d6353cc9861d6db
Kelley Foster <kelley.foster@aaf.edu.au>
2025-03-12 11:53:33 +1000

    Remove script comments and change location

--- docs/bin/update_last_modified.sh
+++ docs/bin/update_last_modified.sh
@@ -1,16 +1,13 @@
-#!/opt/homebrew/bin/bash
+#!/bin/bash
 
 for file in docs/**/*.md; do
-  echo "Processing file: $file"
   last_modified=$(git log -1 --format="%ad" --date=format:"%d %B, %Y" "$file")
-  echo "Last modified date: $last_modified"
   if [ -n "$last_modified" ]; then
     if grep -q "^last_updated:" "$file"; then
       sed -i '' "s/^last_updated:.*/last_updated: $last_modified/" "$file"
     else
       sed -i '' "/^last_updated:/ s/^last_updated:/last_updated: $last_modified/" "$file"
     fi
-    echo "Updated last_updated field in $file"
   else
     echo "No last modified date found for $file"
   fi
