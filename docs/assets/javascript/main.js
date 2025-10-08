9024e54f9f857102812f202e6504f3b130c1c095
Kfoster14 <65216258+Kfoster14@users.noreply.github.com>
2025-10-09 09:05:21 +1000

    Improve front page accessibility (#195)
    * Improve colour contrast
    
    * Add space before link, correct headings
    
    * Change colour back to indigo
    
    * Fix spacing

--- docs/assets/javascript/main.js
+++ docs/assets/javascript/main.js
@@ -68,11 +68,11 @@ $(function() {
         result += '<div class="card-container mx-auto" style="width: 18rem;">';
         result += '  <a class="card-tut-link" href="' + tutorial.url + '">';
         result += '    <div class="card shadow h-100">';
-        result += '      <div class="card-header text-light" style="background-color: #2A004A">';
+        result += '      <div class="card-header text-light" style="background-color: #5F368A">';
         result += '        <div class="card-category">';
         result += '          <small class="title text-uppercase">' + tutorial.level + '</small>';
         if (tutorial.level == 'Series') {
-          result += '            <img src="' + baseurl + '/assets/images/aaflogo.png" class="series' + ' float-right"' + ' alt="AAF Series">';
+          result += '            <img src="' + baseurl + '/assets/images/aaflogo.png" class="series float-right"' + ' alt="AAF Series">';
         }
         result += '        </div>';
         result += '        <h5 class="card-title mb-0">' + tutorial.title + '</h5>';
@@ -102,10 +102,12 @@ $(function() {
     }
     else {
       var noResults = '<div class="card-container col-sm-12 col-lg-10 offset-lg-1 mx-auto">';
-          noResults += '  <div class="card shadow p-2 text-center">';
-          noResults += '    <h3>No Matches</h3>';
-          noResults += '    <p>Can\'t find what you\'re looking for? Make a suggestion or ask our support team for' +
-              ' help by submitting a <a href="https://aaf.freshdesk.com/support/tickets/new">support ticket</a>.</p>';
+          noResults += '  <div class="card shadow p-lg-5 text-center">';
+          noResults += '    <h2 class="my-3">No Matches</h2>';
+          noResults += '    <h3 class="mb-4"><small>Can\'t find what you\'re looking for?</small></h3>';
+          noResults += '    <p>Make a suggestion or ask our support team for help by submitting a ' +
+              ' <a style="padding-bottom: 1px; border-bottom: 1px solid #17a2b8; text-decoration:' +
+              ' none;" href="https://aaf.freshdesk.com/support/tickets/new">support ticket</a>.</p>';
           noResults += '  </div>';
           noResults += '</div>';
       tutorialList.html(noResults);
